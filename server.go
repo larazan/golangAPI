3ec3b12e41365cf1cc6cf2329ab99bf17928b4b5
zamroni <forheron@gmail.com>
2022-02-04 16:15:29 +0700

    go api

---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/larazan/golangAPI/config"
+	"github.com/larazan/golangAPI/controller"
+	"gorm.io/gorm"
+)
+
+var (
+	db             *gorm.DB                  = config.SetupDatabaseConnection()
+	authController controller.AuthController = controller.NewAuthController()
+)
+
+func main() {
+	defer config.CloseDatabaseConnection(db)
+	r := gin.Default()
+
+	authRoutes := r.Group("api/auth")
+	{
+		authRoutes.POST("/login", authController.Login)
+		authRoutes.POST("/register", authController.Register)
+	}
+
+	r.Run()
+}
